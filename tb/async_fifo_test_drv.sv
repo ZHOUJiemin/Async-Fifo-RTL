e6ac11ad1e8de49bc12aea5b2be4a0c6a0bf8079
ZHOUJiemin <jieminzhou1987@gmail.com>
2015-09-10 22:29:57 +0900

    Test Bench 50%
    Created most part of the test bench
    Scoreboard need to be built
    Two drivers need to be separated into a driver and a monitor
    Data need to be randomized

--- tb/async_fifo_test_drv.sv
+++ tb/async_fifo_test_drv.sv
@@ -1,5 +1,20 @@
 //created by jiemin on 20150910
 //asynchronous fifo -- test program class Driver
 class Driver;
+  //properties
+  //virtual interface
+  vfifo_if fifo_if;
+  //mailboxes
+  mailbox #(Tranx) gen2drv; //gen2wrdrv, gen2rddrv
+  //data
+  data test_data[];         //dynamic array, allocate memory for it before using it
+  //other variables
+  int wr_rd;  //transaction type
+  int remain; //data
+  function new(vfifo_if fifo_if,
+               mailbox #(Tranx) gen2drv,
+               data drv2scb[$]);
+  
+  endfunction
 
 endclass
--- tb/async_fifo_test_env.sv
+++ tb/async_fifo_test_env.sv
@@ -1,16 +1,34 @@
 //created by jiemin on 20150910
 //asynchronous fifo -- test program class Environment
 typedef virtual async_fifo_if vfifo_if;
+typedef logic [7:0] data;
+
 //include files
+`include "async_fifo_test_tranx.sv"
+`include "async_fifo_test_cfg.sv"
 `include "async_fifo_test_drv.sv"
 `include "async_fifo_test_scb.sv"
+`include "async_fifo_test_gen.sv"
+
 class Environment;
 
   //properties
   //transactors
+  Generator gen;
   Driver wrdrv;
   Driver rddrv;
   Scoreboard scb;
+
+  //mailboxes and queues
+  mailbox #(Tranx) gen2wrdrv;  //a mailbox used to issue a write transaction
+  mailbox #(Tranx) gen2rddrv;  //a mailbox used to issue a read transaction
+  mailbox #(data) rddrv2scb;   //a mailbox used to send the read data to the scoreboard
+  data wrdrv2scb[$]; //a queue used to send the write data to the scoreboard
+
+  //configuration
+  Config cfg;
+
+  //virtual interfaces
   vfifo_if fifo_if;
 
   //constructor
@@ -27,17 +45,42 @@ endclass
 
 //methods definition
 function void Environment::gen_cfg();
-
+  //generate configuration
+  cfg = new();
+  if(!cfg.randomize())
+    $display("@time %4t  Configuration Randomization Has Failed");
+  else
+  begin
+    $display("@time %4t  Configuration Randomization Done");
+    cfg.display();
+  end
 endfunction
 
 function void Environment::build();
-
+  //build up the mailboxes here
+  gen2wrdrv = new();
+  gen2rddrv = new();
+  rddrv2scb = new();
+  //build up the transactors here
+  gen = new(gen2wrdrv, gen2rddrv, cfg);
+  wrdrv = new(fifo_if.wrdrv_mp, gen2wrdrv, wrdrv2scb); //pass interface by using modports
+  rddrv = new(fifo_if.rddrv_mp, gen2rddrv, rddrv2scb); //pass interface by using modports
+  scb = new(wrdrv2scb, rddrv2scb, cfg); //the scoreboard need to know how many data are transferred
 endfunction
 
 function void Environment::run();
-
+  //get things working here
+  fork
+    gen.run();
+    wrdrv.run();
+    rddrv.run();
+    scb.run();
+  join
 endfunction
 
 function void Environment::wrap();
-
+  //don't know exactly what to do here
+  $display("@time %4t  Test Wrapping Up");
+  //display the result from the scoreboard
+  $display("Good Count = %0d  <-->  Bad Count = %0d", cfg.good_count, cfg.bad_count);
 endfunction
